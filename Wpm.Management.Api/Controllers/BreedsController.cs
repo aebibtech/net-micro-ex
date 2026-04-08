49c10e7e89b0f3e78a9d1e1fd93ce946424d87df
Paul Abib Camano <paul@Pauls-MacBook-Air-8.local>
2026-04-08 10:28:34 +0800

    initial commit

---
+++ Wpm.Management.Api/Controllers/BreedsController.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BreedsController(ManagementDbContext dbContext, ILogger<BreedsController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        try
+        {
+            var all = await dbContext.Breeds
+                .ToListAsync();
+            return Ok(all);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error has occurred while getting Breeds.");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet("{id}", Name = nameof(GetBreedById))]
+    public async Task<IActionResult> GetBreedById (int id)
+    {
+        try
+        {
+            var breed = await dbContext.Breeds
+                .Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (breed == null)
+                return NotFound();
+
+            return Ok(breed);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error has occurred while getting a Breed.");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(NewBreed newBreed)
+    {
+        var breed = newBreed.ToBreed();
+        await dbContext.Breeds.AddAsync(breed);
+        await dbContext.SaveChangesAsync();
+        return CreatedAtRoute(nameof(GetBreedById), new { id = breed.Id }, newBreed);
+    }
+}
+
+public record NewBreed([Required]string Name)
+{
+    public Breed ToBreed()
+    {
+        return new Breed(0, Name);
+    }
+}
---
+++ Wpm.Management.Api/Controllers/PetsController.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PetsController(ManagementDbContext dbContext, ILogger<PetsController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        try
+        {
+            var all = await dbContext.Pets
+                .Include(p => p.Breed)
+                .ToListAsync();
+            return Ok(all);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error has occurred while getting pets.");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet("{id}", Name = nameof(GetById))]
+    public async Task<IActionResult> GetById (int id)
+    {
+        try
+        {
+            var pet = await dbContext.Pets
+                .Include(p => p.Breed)
+                .Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (pet == null)
+                return NotFound();
+
+            return Ok(pet);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error has occurred while getting a pet.");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(NewPet newPet)
+    {
+        var pet = newPet.ToPet();
+        await dbContext.Pets.AddAsync(pet);
+        await dbContext.SaveChangesAsync();
+        return CreatedAtRoute(nameof(GetById), new { id = pet.Id }, newPet);
+    }
+}
+
+public record NewPet([Required]string Name, int Age, int BreedId)
+{
+    public Pet ToPet()
+    {
+        return new Pet() { Name = Name, Age = Age, BreedId = BreedId };
+    }
+}
