9f0e26212c303191b9b85aadfaf3a2bdc8945b1b
Paul Abib Camano <paul@Pauls-MacBook-Air-8.local>
2026-04-08 17:00:58 +0800

    Add Clinic microservice

---
+++ Wpm.Clinic/Application/ClinicApplicationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Wpm.Clinic.Controllers;
+using Wpm.Clinic.DataAccess;
+
+namespace Wpm.Clinic.Application;
+
+public class ClinicApplicationService(ClinicDbContext dbContext, ExternalServices.ManagementService managementService)
+{
+	public async Task<Consultation> Handle(StartConsultationCommand command)
+	{
+		var petInfo = await managementService.GetPetInfo(command.PatientId);
+		var newConsultation = new Consultation(Guid.NewGuid(),
+                                         command.PatientId,
+                                         petInfo.Name,
+                                         petInfo.Age,
+                                         DateTime.UtcNow);
+
+		await dbContext.Consultations.AddAsync(newConsultation);
+		await dbContext.SaveChangesAsync();
+
+		return newConsultation;
+	}
+}
+
---
+++ Wpm.Clinic/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wpm.Clinic.Application;
+using Wpm.Clinic.DataAccess;
+using Wpm.Clinic.ExternalServices;
+using Polly;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ManagementService>();
+builder.Services.AddScoped<ClinicApplicationService>();
+builder.Services.AddDbContext<ClinicDbContext>(options =>
+{
+    options.UseInMemoryDatabase("WpmClinic");
+});
+builder.Services.AddHttpClient<ManagementService>(client =>
+{
+    var uri = builder.Configuration.GetValue<string>("Wpm:ManagementUri");
+    client.BaseAddress = new Uri(uri ?? string.Empty);
+}).AddResilienceHandler("management-pipeline", b =>
+{
+    b.AddRetry(new Polly.Retry.RetryStrategyOptions<HttpResponseMessage>()
+    {
+        BackoffType = DelayBackoffType.Exponential,
+        MaxRetryAttempts = 3,
+        Delay = TimeSpan.FromSeconds(10)
+    });
+});
+
+var app = builder.Build();
+
+app.EnsureClinicDbIsCreated();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
